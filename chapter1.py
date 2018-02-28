d36533e54a90ad26575a12dc49b90551aa569699
rajnisharma30 <rajnisharma_30@yahoo.in>
2018-02-28 14:38:31 +0530

    chapter one

---
+++ chapter1.py
@@ -0,0 +1,32 @@
+# how to print in python
+print ("hi I am python")
+
+print ("comments start with #")
+
+# Variables
+name='rajni'
+age=27
+year=2018
+month='feb'
+distance=11.11
+
+print (name)
+print (age)
+print (year)
+print (month)
+print (distance)
+
+# Multiple Assignment
+a=b=c=1
+print (a)
+print (b)
+print (c)
+
+a,b,c=1,2,3
+print (a)
+print (b)
+print (c)
+
+
+
+
